28677fb2206c06b360e2f65a7e5fef68ffad913e
Viktor <Cezn.Vi@gmail.com>
2023-01-10 20:53:00 +0200

    WIP

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Dictionary
+﻿using System.Text;
+
+namespace Dictionary
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "Dictionary";
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Word word = new();
+            word.BWord = "dog";
+            word.AddTranslete("собака");
+            word.AddTranslete("пес");
+            word.AddTranslete("собак");
+
+            Word word2 = new("cat", "кіт");
+
+            Console.WriteLine(word);
+            word.RemoveTranslete("пес");
+            Console.WriteLine(word2);
+
+            //Console.WriteLine("\n Тестування Гет транслейт");
+            //foreach (string item in word.GetTranslete())
+            //    Console.Write(item + ' ');
+
+
+
+
+
+
         }
     }
 }
---
+++ Dictionary/Word.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dictionary
+{
+
+    /// <summary>
+    /// Контейнер для зберігання слова та перекладів до нього
+    /// </summary>
+    public class Word
+    {
+        /// <summary>
+        /// Властивість для зберігання слова
+        /// </summary>
+        public string BWord { get; set; }
+
+        /// <summary>
+        /// Список для зберігання перекладу слова
+        /// </summary>
+        private List<string> TWord = new List<string>();
+
+        /// <summary>
+        /// Додати переклад до базового слова
+        /// </summary>
+        /// <param name="word">Варіант перекладу</param>
+        public void AddTranslete(string word) { TWord.Add(word); }
+        
+        /// <summary>
+        /// Видалити переклад базавого за слова
+        /// </summary>
+        /// <param name="word">Варіант перекладу</param>
+        public void RemoveTranslete(string word) 
+        { 
+            if(TWord.Count > 1)
+                TWord.Remove(word); 
+        }
+
+        /// <summary>
+        /// Геттер списку переладів
+        /// </summary>
+        /// <returns>Повртає список варіантів перекладу</returns>
+        public List<string> GetTranslete() { return TWord; }
+
+        /// <summary>
+        /// Перевантаженя оператора ТуСтрінг
+        /// </summary>
+        /// <returns>Повертає строку зі словом та варіантами
+        /// перекладу</returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string item in TWord)
+            {
+                if (item != TWord.Last())
+                    sb.Append(item + ", ");
+                else
+                    sb.Append(item);
+            }
+
+            return BWord + " -- " + sb;
+        }
+
+        public Word(string bWord, List<string> tWord)
+        {
+            BWord = bWord;
+            TWord = tWord;
+        }
+
+        public Word(string bWord, string translete) 
+        {
+            BWord = bWord;
+            TWord.Add(translete);
+        }
+
+        public Word() 
+        {
+            BWord = string.Empty;
+        }
+    }
+}
